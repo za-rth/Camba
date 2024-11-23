362b816a6c53eac88df71594378168d2a234328a
zarth <98376279+za-rth@users.noreply.github.com>
2024-11-24 04:00:59 +0800

    YES 5

--- buyerPage.php
+++ buyerPage.php
@@ -1,6 +1,37 @@
 
-
 <?php
-
+    include 'resources/bootstrap&googleFonts.php';
 ?>
-<h3>HELLO</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAMBA</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<header class="contaier navbar navbar-expand-lg navbar-light bg-light"
+      style="font-family: 'Roboto Mono', monospace; font-weight: 100;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="images/CAMBa.png" width="125">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+      </div>
+    </header>
+
+    <main>
+      
+    </main>
+
+    <footer>
+        <p>&copy; 2023 Your Website</p>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require 'config.php';
 include 'functions/login.php';
 include 'functions/signUp.php';
+include 'resources/bootstrap&googleFonts.php';
+
 
 
 
@@ -11,14 +13,6 @@ include 'functions/signUp.php';
 <html lang="en">
 
 <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
-    rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CAMBA</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 </head>
