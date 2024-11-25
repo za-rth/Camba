175da267e4e28f8d8759a7ff82052e3f89764822
zarth <98376279+za-rth@users.noreply.github.com>
2024-11-25 16:16:14 +0800

    YES 12

--- functions/login.php
+++ functions/login.php
@@ -3,28 +3,28 @@ include 'config.php';
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
-
+    $email = $_POST['email'];
+    $password = $_POST['password'];
     if (isset($_POST['login'])) {
 
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+
         if (empty($email) && empty($password)) {
             echo "<script>alert('Insert valid Credentials')</script>";
-           
+
             exit;
         } else {
-            /*$sql = $connection->prepare("SELECT EMAIL FROM `user_account` WHERE `EMAIL` = ? AND `PASSWORD` = ?");
+            $sql = $connection->prepare("SELECT EMAIL FROM `user_account` WHERE `EMAIL` = ? AND `PASSWORD` = ?");
             $sql->bind_param("ss", $email, $password);
             $sql->execute();
             echo "<script>alert('Login Successful')</script>";
             header("Location: buyerPage.php");
-            $sql->close();*/
+            $sql->close();
         }
 
 
     }
-session_unset();
-session_destroy();
+    session_unset();
+    session_destroy();
 
 
 }
